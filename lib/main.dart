40c462a6bee660699953cb572905935df9da6d60
thanhtuyen <tuyenb1910327@student.ctu.edu.vn>
2022-09-13 11:12:10 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
+import 'ui/products/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,20 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'My Shop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.purple,
-          ).copyWith(
-            secondary: Colors.orange,
-          ),
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.orange,
         ),
-        home: SafeArea(
-            child: ProductDetailScreen(
-          ProductsManager().items[0],
-        ),
+      ),
+      home: const SafeArea(
+        child: ProductsOverviewScreen(),
       ),
     );
   }
