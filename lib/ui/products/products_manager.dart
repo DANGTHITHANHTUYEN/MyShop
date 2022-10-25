ae4edd1495e2a426341caab1b7da5e48b797dc08
DANGTHITHANHTUYEN <tuyenb1910327@student.ctu.edu.vn>
2022-10-25 21:09:27 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -2,59 +2,71 @@ import 'package:flutter/foundation.dart';
 import '../../models/product.dart';
 import '../../models/auth_token.dart';
 import '../../services/products_service.dart';
-class ProductsManager with ChangeNotifier{
-  List <Product> _items = [];
+
+class ProductsManager with ChangeNotifier {
+  List<Product> _items = [];
 
   final ProductsService _productsService;
 
   ProductsManager([AuthToken? authToken])
-    : _productsService = ProductsService(authToken);
-  
-  set authToken(AuthToken? authToken){
+      : _productsService = ProductsService(authToken);
+
+  set authToken(AuthToken? authToken) {
     _productsService.authToken = authToken;
   }
 
-  Future<void> fetchProducts([bool filterByUser = false]) async{
+  Future<void> fetchProducts([bool filterByUser = false]) async {
     _items = await _productsService.fetchProducts(filterByUser);
     notifyListeners();
   }
 
-  Future<void> addProduct(Product product) async{
+  Future<void> addProduct(Product product) async {
     final newProduct = await _productsService.addProduct(product);
-    if (newProduct != null){
+    if (newProduct != null) {
       _items.add(newProduct);
       notifyListeners();
     }
   }
+
   int get itemCount {
     return _items.length;
   }
-  List<Product> get items{
+
+  List<Product> get items {
     return [..._items];
   }
-  List<Product> get favoriteItems{
+
+  List<Product> get favoriteItems {
     return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
-  Product findById(String id){
+
+  Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void updateProduct(Product product){
+  Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
-    if(index >= 0){
-      _items[index] = product;
+    if (index >= 0) {
+      if (await _productsService.updateProduct(product)) {
+        _items[index] = product;
+        notifyListeners();
+      }
+    }
+  }
+
+  Future<void> deleteProduct(String id) async {
+    final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
+    _items.removeAt(index);
+    notifyListeners();
+    if (!await _productsService.deleteProduct(id)) {
+      _items.insert(index, existingProduct);
       notifyListeners();
     }
   }
-  void toggleFavoriteStatus(Product product){
+
+  void toggleFavoriteStatus(Product product) {
     final savedStatus = product.isFavorite;
     product.isFavorite = !savedStatus;
   }
-
-  void deleteProduct ( String id){
-    final index = _items.indexWhere((item) => item.id == id);
-    _items.removeAt(index);
-    notifyListeners();
-  }
-
 }
